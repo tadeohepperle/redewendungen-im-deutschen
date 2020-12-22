d8239064ff42eac2885d5766e128f485a91d3746
Tadeo <tadeo@do-mix.de>
2020-12-22 21:32:47 +0100

    description

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ function getRandomSaying()
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=, initial-scale=1.0" />
     <title>Deutsche Redensarten lernen</title>
+    <meta name="description" content="Täglich neue Redewendungen lernen: Tausende deutsche Ausdrücke, die die eigene Sprache verschönern.">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
